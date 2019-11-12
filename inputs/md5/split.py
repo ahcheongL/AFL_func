336a54777b59c839418bf5e13ba8a30a1f33342a
cheong <cheong@tester13.swtv>
2019-11-12 17:29:17 +0900

    update md5sum input

--- inputs/md5/split.py
+++ inputs/md5/split.py
@@ -1,15 +1,20 @@
-import random
 
 inp = open("md5all.txt", "r")
 
 idx = 0
+idx2 = 0
 out = open("md5_" + str(idx) + ".txt", "w")
 for l in inp:
   out.write(l)
-  if random.random() < 1 / 130:
+  idx2 += 1
+  if idx2 > 20:
     out.close()
     idx += 1
+    idx2 = 0
     out = open ("md5_" + str(idx) + ".txt", "w")
+
+  if idx > 5:
+    break
 
 out.close()
 inp.close()
